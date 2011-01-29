d34066a1afe6c2eabbfef0a18f32e71e9c34ea74
Uli Kusterer <ulivc@zathras.de>
2011-01-29 02:39:51 +0100

    ForgeDebugger: Deleted some unneeded, commented-out code.

--- ForgeDebuggerAppDelegate.m
+++ ForgeDebuggerAppDelegate.m
@@ -17,43 +17,6 @@
 @synthesize variablesTable = mVariablesTable;
 @synthesize textView = mTextView;
 
-//	int						sockfd,
-//							portno,
-//							n;
-//    struct	sockaddr_in		serv_addr;
-//    struct	hostent		*	server = NULL;
-//
-//    char buffer[256];
-//    portno = 13762;
-//    sockfd = socket( AF_INET, SOCK_STREAM, 0 );
-//    server = gethostbyname( "127.0.0.1" );
-//    if( server == NULL )
-//	{
-//        fprintf(stderr,"ERROR, no such host\n");
-//        exit(0);
-//    }
-//    bzero((char *) &serv_addr, sizeof(serv_addr));
-//    serv_addr.sin_family = AF_INET;
-//    bcopy((char *)server->h_addr, 
-//         (char *)&serv_addr.sin_addr.s_addr,
-//         server->h_length);
-//    serv_addr.sin_port = htons(portno);
-//    if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0) 
-//        error("ERROR connecting");
-//    printf("Please enter the message: ");
-//    bzero(buffer,256);
-//    fgets(buffer,255,stdin);
-//    n = write(sockfd,buffer,strlen(buffer));
-//    if (n < 0) 
-//         error("ERROR writing to socket");
-//    bzero(buffer,256);
-//    n = read(sockfd,buffer,255);
-//    if (n < 0) 
-//         error("ERROR reading from socket");
-//    printf("%s\n",buffer);
-//    return 0;
-
-
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
